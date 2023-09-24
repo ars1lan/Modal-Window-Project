bddef1a329c2cf2429e18deaf69a3d4c8359acc0
Arslan Ali <sunnysheikh303@gmail.com>
2023-09-24 08:31:25 +0500

    Update the JS according to requirement

--- ProjectFiles/script.js
+++ ProjectFiles/script.js
@@ -1,1 +1,25 @@
 'use strict';
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnOpenModal = document.querySelectorAll('.show-modal');
+const openModel = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+  //we can also do that
+  //modal.style.display = 'block';
+};
+const closeModel = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+for (let i = 0; i < btnOpenModal.length; i++) {
+  btnOpenModal[i].addEventListener('click', openModel);
+}
+overlay.addEventListener('click', closeModel);
+btnCloseModal.addEventListener('click', closeModel);
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModel();
+  }
+});
